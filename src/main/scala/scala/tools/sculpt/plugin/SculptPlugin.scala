04e6bd6732d4d673876040b34fd9b756ec61a51f
Seth Tisue <seth@tisue.net>
2015-12-07 10:49:07 -0800

    add comment

--- src/main/scala/scala/tools/sculpt/plugin/SculptPlugin.scala
+++ src/main/scala/scala/tools/sculpt/plugin/SculptPlugin.scala
@@ -5,6 +5,7 @@ import nsc.Global
 import nsc.plugins.Plugin
 import nsc.plugins.PluginComponent
 
+// after `compile` and `copyResources`,
 // use as `scalac -Xplugin:~/git/scala-sculpt/target/scala-2.11/classes -Xplugin-require:scala-sculpt`
 class SculptPlugin(val global: Global) extends Plugin {
   val name        = "scala-sculpt"
