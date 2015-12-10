1c153cf87a481e2e681b0977a07d85f78ac63dd0
Seth Tisue <seth@tisue.net>
2015-12-09 16:45:16 -0800

    add serialization/deserialization testing

---
+++ src/test/scala/SerializationTests.scala
@@ -0,0 +1,34 @@
+package scala.tools.sculpt
+package model
+
+import org.junit.Test
+import org.junit.Assert.assertEquals
+
+import spray.json._
+
+class SerializationTests {
+
+  // Stefan didn't like the appearance of the output with either of
+  // spray-json's formatters (`compactPrint` and `prettyPrint`),
+  // so he rolled his own in FullDependenciesPrinter
+  def print(js: JsValue): String = {
+    val sb = new java.lang.StringBuilder
+    FullDependenciesPrinter.print(js, sb)
+    sb.toString
+  }
+
+  // JSON -> JSValue -> JSON
+  @Test def roundTripThroughJsonASTs(): Unit = {
+    assertEquals(Samples.json1,
+      print(Samples.json1.parseJson))
+  }
+
+  // JSON -> JSValue -> Seq[FullDependency] -> JSValue -> JSON
+  @Test def roundTripThroughModel(): Unit = {
+    import ModelJsonProtocol._
+    val dependencies =
+      Samples.json1.parseJson.convertTo[Seq[FullDependency]]
+    assertEquals(Samples.json1, print(dependencies.toJson))
+  }
+
+}
