8ecdea405273014bf120dff8ffdbafc9cc7516a5
Seth Tisue <seth@tisue.net>
2016-02-16 18:15:33 -0500

    add comments to ClassMode

--- src/main/scala/com/typesafe/tools/sculpt/model/ClassMode.scala
+++ src/main/scala/com/typesafe/tools/sculpt/model/ClassMode.scala
@@ -3,25 +3,41 @@
 package com.typesafe.tools.sculpt.model
 
 object ClassMode {
+
+  // promotes all of the dependencies to class level, discarding self-dependencies,
+  // collapsing the uses/extends distinction, setting all counts to 1, and eliminating
+  // duplicates. (real counts handling could be a future todo.)  sorting the output
+  // helps testability and helps human-readability of the resulting JSON.
+
   def apply(deps: Seq[FullDependency]): Seq[FullDependency] = {
-    def promote(path: Path): Option[Path] = {
-      val (packages, rest) = path.elems.span(_.kind == EntityKind.PackageType)
-      if (rest.nonEmpty && isClassKind(rest.head.kind))
-        Some(Path(packages :+ rest.head))
-      else
-        None
-    }
-    val candidates =
+    val promoted =
       for {
         dep <- deps
         from <- promote(dep.from)
         to <- promote(dep.to)
         if from != to
       } yield FullDependency(from = from, to = to, kind = DependencyKind.Uses, count = 1)
-    candidates.distinct
-      .sortBy(_.toString)  // helps human-readability
+    promoted.distinct.sortBy(_.toString)
   }
+
+  // Throws away too-specific stuff at the end of a path, so we end up with
+  // a class-level path (or None).  The too-specific stuff might be a method,
+  // inner class, etc.
+
+  def promote(path: Path): Option[Path] = {
+    val (packages, rest) = path.elems.span(_.kind == EntityKind.PackageType)
+    if (rest.nonEmpty && isClassKind(rest.head.kind))
+      Some(Path(packages :+ rest.head))
+    else
+      None
+  }
+
+  // The inclusion of EntityKind.Type may seem questionable, but it's needed
+  // in order to pull in things like `extends scala.AnyRef` since AnyRef is
+  // just a type alias for `java.lang.Object`
+
   private val isClassKind: EntityKind => Boolean =
     Set[EntityKind](
       EntityKind.Trait, EntityKind.Class, EntityKind.ModuleClass, EntityKind.Type)
+
 }
