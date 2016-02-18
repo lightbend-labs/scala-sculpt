b96db2acf93b30e013d5a68907faa1721fdc0c31
Seth Tisue <seth@tisue.net>
2016-02-17 19:11:27 -0500

    minor code improvement

--- src/test/scala/com/typesafe/tools/sculpt/ComponentsTests.scala
+++ src/test/scala/com/typesafe/tools/sculpt/ComponentsTests.scala
@@ -8,12 +8,14 @@ import org.scalatest.FunSuite
 object ComponentsTests {
   // also used by Samples.main
   def toCycleString(name: String, json: String): String = {
-    import spray.json._
-    import ModelJsonProtocol._
-    val deps = json.parseJson.convertTo[Seq[FullDependency]]
-    val classJson = FullDependenciesPrinter.print(ClassMode(deps).toJson)
-    val graph = GraphTests.toGraph(name, classJson)
-    (new Components)(graph.nodes)(_.edgesOut.map(_.to))
+    val nodes: Iterable[Node] = {
+      import spray.json._
+      import ModelJsonProtocol._
+      val deps = json.parseJson.convertTo[Seq[FullDependency]]
+      val classJson = FullDependenciesPrinter.print(ClassMode(deps).toJson)
+      GraphTests.toGraph(name, classJson).nodes
+    }
+    (new Components)(nodes)(_.edgesOut.map(_.to))
       .filter(_.size > 1)
       .map(_.toSeq.sortBy(_.toString).mkString(" "))
       .sortBy(_.toString)
@@ -22,11 +24,10 @@ object ComponentsTests {
 }
 
 class ComponentsTests extends FunSuite {
-  for {
-    sample <- Samples.samples
-  } test(sample.name) {
-    assertResult(sample.cycles) {
-      ComponentsTests.toCycleString(sample.name, sample.json)
+  for (sample <- Samples.samples)
+    test(sample.name) {
+      assertResult(sample.cycles) {
+        ComponentsTests.toCycleString(sample.name, sample.json)
+      }
     }
-  }
 }
